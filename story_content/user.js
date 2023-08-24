562e4d32b112b6c55dcd7ef55104ac6008eb8785
Majenta555 <majenta@gmail.com>
2023-08-24 11:05:39 +1000

    Klatt Test Branching Scenario

---
+++ story_content/user.js
@@ -0,0 +1,238 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "66Wxacl22cV":
+        Script1();
+        break;
+      case "6MIwQRhdpoo":
+        Script2();
+        break;
+      case "5x90SNOGVf5":
+        Script3();
+        break;
+      case "5VvxZl8ZYgG":
+        Script4();
+        break;
+      case "5gmGgTypoRt":
+        Script5();
+        break;
+      case "5W8j1FQkN8J":
+        Script6();
+        break;
+      case "6gesq68YZpi":
+        Script7();
+        break;
+      case "6h1ii5bGu1i":
+        Script8();
+        break;
+      case "6kN8gIzxZFM":
+        Script9();
+        break;
+      case "6c4617ROPZm":
+        Script10();
+        break;
+      case "5n0QBjD9pCF":
+        Script11();
+        break;
+      case "6HHRo36a71k":
+        Script12();
+        break;
+      case "6JCE1bIiEke":
+        Script13();
+        break;
+      case "6r7ndiCtneX":
+        Script14();
+        break;
+  }
+}
+
+function Script1()
+{
+  var duration = 5 * 1000;
+var animationEnd = Date.now() + duration;
+var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+function randomInRange(min, max) {
+  return Math.random() * (max - min) + min;
+}
+var interval = setInterval(function() {
+  var timeLeft = animationEnd - Date.now();
+  if (timeLeft <= 0) {
+    return clearInterval(interval);
+  }
+  var particleCount = 50 * (timeLeft / duration);
+  // since particles fall down, start a bit higher than random
+  confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } }));
+  confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } }));
+}, 250);
+}
+
+function Script2()
+{
+  var confettiScript = document.createElement('script');
+confettiScript.setAttribute('src','https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js');
+document.head.appendChild(confettiScript);
+}
+
+function Script3()
+{
+  var duration = 5 * 1000;
+var animationEnd = Date.now() + duration;
+var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+function randomInRange(min, max) {
+  return Math.random() * (max - min) + min;
+}
+var interval = setInterval(function() {
+  var timeLeft = animationEnd - Date.now();
+  if (timeLeft <= 0) {
+    return clearInterval(interval);
+  }
+  var particleCount = 50 * (timeLeft / duration);
+  // since particles fall down, start a bit higher than random
+  confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } }));
+  confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } }));
+}, 250);
+}
+
+function Script4()
+{
+  var confettiScript = document.createElement('script');
+confettiScript.setAttribute('src','https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js');
+document.head.appendChild(confettiScript);
+}
+
+function Script5()
+{
+  var duration = 5 * 1000;
+var animationEnd = Date.now() + duration;
+var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+function randomInRange(min, max) {
+  return Math.random() * (max - min) + min;
+}
+var interval = setInterval(function() {
+  var timeLeft = animationEnd - Date.now();
+  if (timeLeft <= 0) {
+    return clearInterval(interval);
+  }
+  var particleCount = 50 * (timeLeft / duration);
+  // since particles fall down, start a bit higher than random
+  confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } }));
+  confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } }));
+}, 250);
+}
+
+function Script6()
+{
+  var confettiScript = document.createElement('script');
+confettiScript.setAttribute('src','https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js');
+document.head.appendChild(confettiScript);
+}
+
+function Script7()
+{
+  var duration = 5 * 1000;
+var animationEnd = Date.now() + duration;
+var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+function randomInRange(min, max) {
+  return Math.random() * (max - min) + min;
+}
+var interval = setInterval(function() {
+  var timeLeft = animationEnd - Date.now();
+  if (timeLeft <= 0) {
+    return clearInterval(interval);
+  }
+  var particleCount = 50 * (timeLeft / duration);
+  // since particles fall down, start a bit higher than random
+  confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } }));
+  confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } }));
+}, 250);
+}
+
+function Script8()
+{
+  var confettiScript = document.createElement('script');
+confettiScript.setAttribute('src','https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js');
+document.head.appendChild(confettiScript);
+}
+
+function Script9()
+{
+  var duration = 5 * 1000;
+var animationEnd = Date.now() + duration;
+var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+function randomInRange(min, max) {
+  return Math.random() * (max - min) + min;
+}
+var interval = setInterval(function() {
+  var timeLeft = animationEnd - Date.now();
+  if (timeLeft <= 0) {
+    return clearInterval(interval);
+  }
+  var particleCount = 50 * (timeLeft / duration);
+  // since particles fall down, start a bit higher than random
+  confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } }));
+  confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } }));
+}, 250);
+}
+
+function Script10()
+{
+  var confettiScript = document.createElement('script');
+confettiScript.setAttribute('src','https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js');
+document.head.appendChild(confettiScript);
+}
+
+function Script11()
+{
+  var duration = 5 * 1000;
+var animationEnd = Date.now() + duration;
+var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+function randomInRange(min, max) {
+  return Math.random() * (max - min) + min;
+}
+var interval = setInterval(function() {
+  var timeLeft = animationEnd - Date.now();
+  if (timeLeft <= 0) {
+    return clearInterval(interval);
+  }
+  var particleCount = 50 * (timeLeft / duration);
+  // since particles fall down, start a bit higher than random
+  confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } }));
+  confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } }));
+}, 250);
+}
+
+function Script12()
+{
+  var confettiScript = document.createElement('script');
+confettiScript.setAttribute('src','https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js');
+document.head.appendChild(confettiScript);
+}
+
+function Script13()
+{
+  var duration = 5 * 1000;
+var animationEnd = Date.now() + duration;
+var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+function randomInRange(min, max) {
+  return Math.random() * (max - min) + min;
+}
+var interval = setInterval(function() {
+  var timeLeft = animationEnd - Date.now();
+  if (timeLeft <= 0) {
+    return clearInterval(interval);
+  }
+  var particleCount = 50 * (timeLeft / duration);
+  // since particles fall down, start a bit higher than random
+  confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } }));
+  confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } }));
+}, 250);
+}
+
+function Script14()
+{
+  var confettiScript = document.createElement('script');
+confettiScript.setAttribute('src','https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js');
+document.head.appendChild(confettiScript);
+}
+
